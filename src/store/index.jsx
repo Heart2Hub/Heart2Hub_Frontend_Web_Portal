c426cab0c9be6fb48f483453da6519945c0a44cb
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2023-09-17 15:53:00 +0800

    IHIS-110
    redux persist working, currently store in localstorage is being persisted correctly and rehydrating correctly on refresh

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,20 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // Default: localStorage for web
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 import staffReducer from "./slices/staffSlice";
 import snackbarReducer from "./slices/snackbarSlice";
@@ -12,27 +26,28 @@ const persistConfig = {
   storage,
 };
 
-const rootReducer = {
+const rootReducer = combineReducers({
   staff: staffReducer,
   snackbar: snackbarReducer,
   loadingOverlay: loadingOverlayReducer,
-};
+});
 
-const persistedReducer = {};
-for (let key in rootReducer) {
-  persistedReducer[key] = persistReducer(persistConfig, rootReducer[key]);
-}
+// const persistedReducer = {};
+// for (let key in rootReducer) {
+//   persistedReducer[key] = persistReducer(persistConfig, rootReducer[key]);
+// }
+// console.log(persistedReducer);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [
-        "persist/PERSIST",
-        "persist/REHYDRATE" /* any other redux-persist actions */,
-      ],
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 let persistor = persistStore(store);
