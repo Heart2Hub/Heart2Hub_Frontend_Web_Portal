8c168c24107a217a9ec3cedf813866bd507526bb
amelia <ameliachong.23@gmail.com>
2023-09-18 10:19:06 +0800

    /api change to /leave

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -38,25 +38,25 @@ export const leaveApi = {
   getAllManagedLeaves(staffId) {
     console.log(staffId);
     return axiosFetch.get(
-      `${REST_ENDPOINT}/api/getAllManagedLeaves?staffId=${staffId}`
+      `${REST_ENDPOINT}/leave/getAllManagedLeaves?staffId=${staffId}`
     );
   },
   getLeaveBalance(staffId) {
     console.log(staffId);
     return axiosFetch.get(
-      `${REST_ENDPOINT}/api/getLeaveBalance?staffId=${staffId}`
+      `${REST_ENDPOINT}/leave/getLeaveBalance?staffId=${staffId}`
     );
   },
   approveLeaveDate(leaveId) {
     console.log(leaveId);
     return axiosFetch.put(
-      `${REST_ENDPOINT}/api/approveLeaveDate?leaveId=${leaveId}`
+      `${REST_ENDPOINT}/leave/approveLeaveDate?leaveId=${leaveId}`
     );
   },
   rejectLeaveDate(leaveId) {
     console.log(leaveId);
     return axiosFetch.put(
-      `${REST_ENDPOINT}/api/rejectLeaveDate?leaveId=${leaveId}`
+      `${REST_ENDPOINT}/leave/rejectLeaveDate?leaveId=${leaveId}`
     );
   }
 }
