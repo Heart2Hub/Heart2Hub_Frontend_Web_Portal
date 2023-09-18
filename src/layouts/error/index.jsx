c5c082f944dedb3be2ae7fd8ce2b8c7e9fccfe39
amelia <ameliachong.23@gmail.com>
2023-09-19 00:50:08 +0800

    Home page, Error Page, Logo, sideNav

--- src/layouts/error/index.jsx
+++ src/layouts/error/index.jsx
@@ -1,7 +1,9 @@
-import { Button } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import error404 from '../../assets/projectImages/error404.gif';
+import MDButton from "components/MDButton";
 
 function ErrorPage() {
   const navigate = useNavigate();
@@ -22,13 +24,39 @@ function ErrorPage() {
   };
 
   return (
-    <>
-      <DashboardLayout>
-        ERROR 404 Oops looks like theres no where to go Click here to return to
-        the home page
-        <Button onClick={handleReroute}>Click me</Button>
-      </DashboardLayout>
-    </>
+    <DashboardLayout>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh'
+        }}
+      >
+        <Container maxWidth="md">
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+              <Typography variant="h1">
+                OOPS!
+              </Typography>
+              <Typography variant="h6">
+                Looks like there is no where to go, click here to return to the home page
+              </Typography>
+              <div style={{ marginTop: '20px' }}>
+                <MDButton variant="contained" onClick={(handleReroute)}>To Home</MDButton>
+              </div>
+            </Grid>
+            <Grid xs={6}>
+              <img
+                src={error404}
+                alt=""
+                width={500} height={400}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </DashboardLayout>
   );
 }
 
