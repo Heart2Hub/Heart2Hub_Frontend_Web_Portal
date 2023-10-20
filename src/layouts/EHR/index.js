b030afb3cb65ce249e5890510838ceed1b9ef7c8
ryks4lyfe <rykerynwa@gmail.com>
2023-10-20 20:59:11 +0800

    merge

--- src/layouts/EHR/index.js
+++ src/layouts/EHR/index.js
@@ -28,7 +28,6 @@ import { maskNric } from "utility/Utility";
 
 function EHR() {
   const navigate = useNavigate();
-  const navigate = useNavigate();
   const reduxDispatch = useDispatch();
 
   const [imageURLs, setImageURLs] = useState({});
