bd2b9127d811bb69ddb7559ece5012c17120bc64
amelia <ameliachong.23@gmail.com>
2023-10-20 20:46:44 +0800

    linked ServiceItem to Unit

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -545,9 +545,9 @@ export const inventoryApi = {
       requestBody
     );
   },
-  createServiceItem(requestBody) {
+  createServiceItem(unitId, requestBody) {
     return axiosFetch.post(
-      `${REST_ENDPOINT}/serviceItem/createServiceItem`,
+      `${REST_ENDPOINT}/serviceItem/createServiceItem?unitId=${unitId}`,
       requestBody
     );
   },
