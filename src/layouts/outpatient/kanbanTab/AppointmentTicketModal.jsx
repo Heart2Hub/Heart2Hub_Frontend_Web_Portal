c42019a180e72edc7909aa1ee54898d54df756a6
amelia <ameliachong.23@gmail.com>
2023-10-27 12:38:54 +0800

    add medication for prescription can search

--- src/layouts/outpatient/kanbanTab/AppointmentTicketModal.jsx
+++ src/layouts/outpatient/kanbanTab/AppointmentTicketModal.jsx
@@ -24,7 +24,8 @@ import {
   Dialog,
   DialogContent,
   DialogActions,
-  DialogContentText
+  DialogContentText,
+  Autocomplete
 } from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
 
@@ -345,27 +346,29 @@ function AppointmentTicketModal({
 
   const renderMedicationsDropdown = () => {
     return (
-      <Box style={{ width: "100%" }}>
-        <InputLabel id="medication-label"> Select Medication</InputLabel>
-        <Select
-          onChange={(e) => setSelectedMedication(e.target.value)}
-          style={{ width: "50%" }}
+      <Box style={{ width: "100%", display: "flex", alignItems: "center" }}>
+        <Autocomplete
+          disablePortal
+          id="medication-label"
+          options={medications}
+          getOptionLabel={(option) => option.inventoryItemName}
+          style={{ width: "80%" }}
           sx={{ lineHeight: "3em" }}
-        >
-          {medications.map((medication) => (
-            <MenuItem key={medication.inventoryItemId} value={medication}>
-              {medication.inventoryItemName}
-            </MenuItem>
-          ))}
-        </Select>
+          renderInput={(params) => <TextField {...params} label="Select Medication" />}
+          value={selectedMedication}
+          onChange={(event, newValue) => {
+            setSelectedMedication(newValue);
+          }}
+        />
         <TextField
           label="Quantity"
           type="number"
           value={selectedMedicationQuantity}
           onChange={(e) => setSelectedMedicationQuantity(e.target.value)}
-          style={{ width: "10%", marginLeft: 20 }}
+          style={{ width: "20%", marginLeft: 10 }}
         />
-        <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}>
+        <ListItem
+          sx={{ display: "flex", justifyContent: "flex-end" }}>
           <MDButton
             onClick={() => handleAddMedicationToPatient(selectedMedication)}
             variant="gradient"
@@ -373,7 +376,9 @@ function AppointmentTicketModal({
           >
             Add Medication
           </MDButton>
-        </Box>
+        </ListItem>
+        {/* <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: 20 }}> */}
+        {/* </Box> */}
       </Box>
     );
   };
@@ -1062,9 +1067,9 @@ function AppointmentTicketModal({
         assigningToSwimlane={columnName}
       />
     </>
-    
+
   );
-  
+
 }
 
 export default AppointmentTicketModal;
